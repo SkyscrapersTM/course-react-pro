5e07aa9335807e78be5d600aceca258973ca18b2
SkyscrapersTM <74199653+SkyscrapersTM@users.noreply.github.com>
2023-07-07 19:46:37 -0500

    initial-project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Navigation from "./routes/Navigation";
+
+function App() {
+  return (
+    <>
+      <Navigation />
+    </>
+  );
+}
+
+export default App;
